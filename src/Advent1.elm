0802faeca201716d91eb9f652b8816e453f479da
Ben Attenborough <battenborough@gmail.com>
2022-12-03 13:27:25 +0000

    Solution for day 1 part 1

--- src/Advent1.elm
+++ src/Advent1.elm
@@ -1,7 +1,10 @@
 module Advent1 exposing (..)
 
-import Advent1Data exposing (test)
-import Parser exposing ((|.), (|=), Parser, Step, end, int, loop, map, oneOf, spaces, succeed, symbol)
+import Advent1Data exposing (..)
+import Html exposing (b)
+import List exposing (foldl)
+import Maybe.Extra as Maybe
+import Parser exposing (..)
 
 
 final : String
@@ -29,14 +32,156 @@ valuesParser =
 valuesParserHelp : List Int -> Parser (Step (List Int) (List Int))
 valuesParserHelp list =
     oneOf
-        [ succeed (\int -> Parser.Loop (int :: list))
+        [ succeed (\int -> Loop (int :: list))
             |= int
             |. spaces
         , succeed ()
-            |> map (\_ -> Parser.Done (List.reverse list))
+            |> map (\_ -> Done (List.reverse list))
         ]
 
 
 resultTwo : Result (List Parser.DeadEnd) (List Int)
 resultTwo =
     Parser.run valuesParser test
+
+
+block : Parser (List Int)
+block =
+    Parser.sequence
+        { start = ""
+        , separator = "/n"
+        , end = "/n/n"
+        , spaces = spaces
+        , item = int
+        , trailing = Optional -- demand a trailing semi-colon
+        }
+
+
+resultThree : Result (List Parser.DeadEnd) (List Int)
+resultThree =
+    Parser.run block test
+
+
+thoughtSeq : String
+thoughtSeq =
+    "1234m5678m9012mm3456m7891"
+
+
+convertToList : String -> List String
+convertToList string =
+    String.lines string
+
+
+
+-- finaliseTest : List (Maybe Int)
+
+
+blockTwo : Parser (List Int)
+blockTwo =
+    Parser.sequence
+        { start = "[,"
+        , separator = ","
+        , end = "]"
+        , spaces = spaces
+        , item = int
+        , trailing = Optional -- demand a trailing semi-colon
+        }
+
+
+looper : Parser (List (List Int))
+looper =
+    loop [] loopHelper
+
+
+loopHelper revStmts =
+    oneOf
+        [ succeed (\stmt -> Loop (stmt :: revStmts))
+            |= blockTwo
+            |. spaces
+            |. symbol ";"
+            |. spaces
+        , succeed ()
+            |> map (\_ -> Done (List.reverse revStmts))
+        ]
+
+
+finaliseTest =
+    String.lines realData
+        |> List.map
+            (\string ->
+                if string == "" then
+                    "];["
+
+                else
+                    string
+            )
+        |> List.intersperse ","
+        |> List.foldr String.append ""
+        |> (++) "[,"
+        |> (\string -> string ++ ",];")
+        |> Parser.run looper
+        |> Result.map (List.map (foldl (+) 0))
+
+
+
+-- |> Result.map
+--     (foldl
+--         (\a b ->
+--             if b > a then
+--                 b
+--             else
+--                 a
+--         )
+--         0
+--     )
+-- |> Parser.run blockTwo
+-- |> List.map String.toInt
+-- |> foldl
+--     (\a b ->
+--         case a of
+--             [] ->
+--                 a
+--             _ ->
+--                 a
+--     )
+--     []
+-- var : Parser String
+-- var =
+--     getChompedString <|
+--         succeed ()
+--             |. chompIf Char.isDigit
+--             |. chompWhile Char.isDigit
+
+
+intsSeperatedByNumbers =
+    succeed String.toInt
+        |= (getChompedString <| chompWhile Char.isDigit)
+        |. spaces
+        |. symbol "m"
+        |. spaces
+
+
+newTest =
+    Parser.run intsSeperatedByNumbers "1234m5678m9012mm3456m7891"
+
+
+statements : Parser (List String)
+statements =
+    loop [] statementsHelp
+
+
+statementsHelp : List String -> Parser (Step (List String) (List String))
+statementsHelp revStmts =
+    oneOf
+        [ succeed (\stmt -> Loop (stmt :: revStmts))
+            |= (getChompedString <| chompWhile Char.isDigit)
+            |. spaces
+            |. symbol "m"
+            |. spaces
+        , succeed ()
+            |> map (\_ -> Done (List.reverse revStmts))
+        ]
+
+
+textX =
+    Parser.run statements "1234m5678m9012mm3456m7891m"
---
+++ src/PhoneParser.elm
@@ -0,0 +1,66 @@
+module PhoneParser exposing (..)
+
+import Parser exposing (..)
+
+
+whitespace : Parser ()
+whitespace =
+    chompWhile (\c -> c == ' ')
+
+
+countryCode : Parser Int
+countryCode =
+    succeed identity
+        |. spaces
+        |. symbol "+"
+        |= int
+        |. spaces
+
+
+countryCodeOptional : Parser (Maybe Int)
+countryCodeOptional =
+    oneOf
+        [ succeed Just
+            |. whitespace
+            |. symbol "+"
+            |= int
+            |. whitespace
+        , succeed Nothing
+        ]
+
+
+runCountryCode : String -> Result (List Parser.DeadEnd) Int
+runCountryCode code =
+    run countryCode code
+
+
+runCountryCodeOptional : String -> Result (List Parser.DeadEnd) (Maybe Int)
+runCountryCodeOptional code =
+    run countryCodeOptional code
+
+
+
+-- > runCountryCodeOptional "+34445"
+-- Ok (Just 34445)
+--     : Result (List Parser.DeadEnd) (Maybe Int)
+-- > runCountryCodeOptional "34445"
+-- Ok Nothing : Result (List Parser.DeadEnd) (Maybe Int)
+
+
+areaCode : Parser (Maybe Int)
+areaCode =
+    oneOf
+        [ succeed String.toInt
+            |. symbol "("
+            |. whitespace
+            |= (getChompedString <| chompWhile Char.isDigit)
+            |. whitespace
+            |. symbol ")"
+            |. whitespace
+        , succeed Nothing
+        ]
+
+
+runAreaCode : String -> Result (List Parser.DeadEnd) (Maybe Int)
+runAreaCode code =
+    run areaCode code
