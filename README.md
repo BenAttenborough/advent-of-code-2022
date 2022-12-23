23e93e9acee22a9eed4632cda6a4b88b5b0ffb17
Ben Attenborough <battenborough@gmail.com>
2022-12-23 16:21:24 +0000

    Add elm-watch explanation to readme

--- README.md
+++ README.md
@@ -3,6 +3,67 @@
 You can import files and run functions `elm repl`
 E.g. `import Advent1`
 
+## Developing in a browser
+
+### TLDR;
+
+Configure a html file, link it to js file (maybe not generated yet)
+Configure `elm.watch.json` to point to js file
+Run:
+
+```bash
+npx elm-watch hot
+```
+
+### Explanation
+
+I've added a package called elm-watch, it uses an `elm-watch.json` file to compile the elm into js AND reload any html file which links to the js.
+
+A minimum setup might be:
+
+```html
+<!DOCTYPE html>
+<html>
+  <head>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    />
+    <link rel="stylesheet" href="base.css" />
+  </head>
+
+  <body>
+    <div id="elm-app-is-loaded-here"></div>
+
+    <script src="advent8.js"></script>
+    <script>
+      var app = Elm.Advent8.init({
+        node: document.getElementById("elm-app-is-loaded-here"),
+      });
+    </script>
+  </body>
+</html>
+```
+
+elm-watch.json
+
+```json
+{
+  "targets": {
+    "My target name": {
+      "inputs": ["src/Advent8.elm"],
+      "output": "html/advent8.js"
+    }
+  }
+}
+```
+
+Then run
+
+```bash
+npx elm-watch hot
+```
+
 ## Tests
 
 To run tests run `elm-test` or `elm-test --watch`
--- src/Advent7.elm
+++ src/Advent7.elm
@@ -1,120 +1,105 @@
 module Advent7 exposing (..)
 
-import Parser exposing (..)
+import Html exposing (Html)
 
 
-type Command
-    = CD
-    | LS
-
-
-type Directory
-    = Empty String Int
-    | Node String Int (List Directory)
+main : Html msg
+main =
+    "Hello Worldx"
+        |> Html.text
 
 
 
--- type File
---     = File Int String
+-- import Parser exposing (..)
+-- type Command
+--     = CD
+--     | LS
 -- type Directory
---     = Directory String
--- type FileType
---     = DirectoryObject Directory
---     | FileObject File
--- type alias DirectoryContent =
---     { files : List File
---     , directories : List Directory
---     }
--- fileParser : Parser DirectoryContent
--- fileParser =
+--     = Empty String Int
+--     | Node String Int (List Directory)
+-- -- type File
+-- --     = File Int String
+-- -- type Directory
+-- --     = Directory String
+-- -- type FileType
+-- --     = DirectoryObject Directory
+-- --     | FileObject File
+-- -- type alias DirectoryContent =
+-- --     { files : List File
+-- --     , directories : List Directory
+-- --     }
+-- -- fileParser : Parser DirectoryContent
+-- -- fileParser =
+-- --     succeed identity
+-- --         |
+-- fileSizeParser : Parser Int
+-- fileSizeParser =
 --     succeed identity
---         |
-
-
-fileSizeParser : Parser Int
-fileSizeParser =
-    succeed identity
-        |= int
-
-
-commandParser : Parser Command
-commandParser =
-    succeed identity
-        |. symbol "$"
-        |. spaces
-        |= oneOf
-            [ succeed CD
-                |. keyword "cd"
-            , succeed LS
-                |. keyword "ls"
-            ]
-
-
-
--- lineParser : Parser a
--- lineParser =
+--         |= int
+-- commandParser : Parser Command
+-- commandParser =
 --     succeed identity
---         |= oneOf
---             [ commandParser
---             , fileSizeParser
---             ]
--- commandParser : Parser Command
--- commandParser =
---     oneOf
---         [ map (\_ -> CD) (keyword "cd")
---         , map (\_ -> LS) (keyword "ls")
---         ]
--- commandParser : Parser Command
--- commandParser =
---     oneOf
---         [ map (\_ -> CD) (keyword "CD")
---         , map (\_ -> LS) (keyword "LS")
---         ]
--- commandParser : Parser Command
--- commandParser =
---     succeed Command
+--         |. symbol "$"
+--         |. spaces
 --         |= oneOf
 --             [ succeed CD
---                 |. keyword "CD"
+--                 |. keyword "cd"
+--             , succeed LS
+--                 |. keyword "ls"
 --             ]
--- day7PartOne : String -> Int
-
-
-day7PartOne input =
-    input
-        |> String.lines
-
-
-directoryA : Directory
-directoryA =
-    Node "/"
-        56
-        [ Node "a"
-            2
-            [ Empty "b1" 5
-            , Empty "b2" 15
-            , Node "b3"
-                23
-                [ Empty "c1" 3 ]
-            ]
-        ]
-
-
-type alias State =
-    { currentDir : String
-    , size : Int
-    , children : List Directory
-    }
-
-
-
--- createDirectory
-
-
-testFunction : List String -> String
-testFunction entries =
-    let
-        finish entry next =
-            next (entry :: entries)
-    in
-    "abc"
+-- -- lineParser : Parser a
+-- -- lineParser =
+-- --     succeed identity
+-- --         |= oneOf
+-- --             [ commandParser
+-- --             , fileSizeParser
+-- --             ]
+-- -- commandParser : Parser Command
+-- -- commandParser =
+-- --     oneOf
+-- --         [ map (\_ -> CD) (keyword "cd")
+-- --         , map (\_ -> LS) (keyword "ls")
+-- --         ]
+-- -- commandParser : Parser Command
+-- -- commandParser =
+-- --     oneOf
+-- --         [ map (\_ -> CD) (keyword "CD")
+-- --         , map (\_ -> LS) (keyword "LS")
+-- --         ]
+-- -- commandParser : Parser Command
+-- -- commandParser =
+-- --     succeed Command
+-- --         |= oneOf
+-- --             [ succeed CD
+-- --                 |. keyword "CD"
+-- --             ]
+-- -- day7PartOne : String -> Int
+-- day7PartOne input =
+--     input
+--         |> String.lines
+-- directoryA : Directory
+-- directoryA =
+--     Node "/"
+--         56
+--         [ Node "a"
+--             2
+--             [ Empty "b1" 5
+--             , Empty "b2" 15
+--             , Node "b3"
+--                 23
+--                 [ Empty "c1" 3 ]
+--             ]
+--         ]
+-- type alias State =
+--     { currentDir : String
+--     , size : Int
+--     , children : List Directory
+--     }
+-- -- createDirectory
+-- testFunction : List String -> String
+-- testFunction entries =
+--     let
+--         finish entry next =
+--             next (entry :: entries)
+--     in
+--     "abc"
