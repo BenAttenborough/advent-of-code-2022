b29028a265d2c5580e5f62f648b413f646999909
Ben Attenborough <ben.attenborough@featurespace.co.uk>
2023-02-28 13:40:03 +0000

    Initial day 11 files

---
+++ tests/Advent11Test.elm
@@ -0,0 +1,20 @@
+module Advent11Test exposing (..)
+
+import Advent11 exposing (..)
+import Advent11Data exposing (realInput, testInput)
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Advent Day 11"
+        [ describe "Part 1" <|
+            [ test "Hello World" <|
+                \_ ->
+                    Expect.equal
+                        1
+                        1
+            ]
+        ]
