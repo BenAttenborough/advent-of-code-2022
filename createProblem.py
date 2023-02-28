d885b07331e6771c6a71a0b8710f313dd79db7ac
Ben Attenborough <battenborough@gmail.com>
2023-02-28 22:42:09 +0000

    Work on code gen

--- createProblem.py
+++ createProblem.py
@@ -1,19 +1,37 @@
 from jinja2 import Environment, FileSystemLoader
 import sys
+from os import listdir
+from os.path import isfile, join
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 moduleName = sys.argv[1]
 template = environment.get_template("Problem.elm")
 
-with open("src/" + moduleName + ".elm", mode="w", encoding="utf-8") as message:
-    message.write(template.render(name=moduleName))
+# with open("src/" + moduleName + ".elm", mode="w", encoding="utf-8") as message:
+#     message.write(template.render(name=moduleName))
 
-testTemplate = environment.get_template("Test.elm")
+# testTemplate = environment.get_template("Test.elm")
 
-with open("tests/" + moduleName + "Test.elm", mode="w", encoding="utf-8") as message:
-    message.write(testTemplate.render(name=moduleName))
+# with open("tests/" + moduleName + "Test.elm", mode="w", encoding="utf-8") as message:
+#     message.write(testTemplate.render(name=moduleName))
 
-dataTemplate = environment.get_template("Data.elm")
+# dataTemplate = environment.get_template("Data.elm")
 
-with open("src/" + moduleName + "Data.elm", mode="w", encoding="utf-8") as message:
-    message.write(dataTemplate.render(name=moduleName))
+# with open("src/" + moduleName + "Data.elm", mode="w", encoding="utf-8") as message:
+#     message.write(dataTemplate.render(name=moduleName))
+
+htmlTemplate = environment.get_template("advent.html")
+
+with open("html/" + moduleName + ".html", mode="w", encoding="utf-8") as message:
+    message.write(htmlTemplate.render(name=moduleName))
+
+onlyFiles = [f for f in listdir("src/") if isfile(join("src/", f))]
+files = ""
+for file in onlyFiles:
+    files += "\"src/" + file + "\","
+files = files.rstrip(",")
+
+elmWatchTemplate = environment.get_template("elm-watch.json")
+
+with open("elm-watch.json", mode="w", encoding="utf-8") as message:
+    message.write(elmWatchTemplate.render(files=files))
--- templates/Problem.elm
+++ templates/Problem.elm
@@ -1,1 +1,16 @@
 module {{ name }} exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+view model =
+    div []
+        [ pre [ style "white-space" "pre-line" ]
+            [ text "TEST\n\n"
+            ]
+        ]
+
+
+main =
+    view "dummy model"
