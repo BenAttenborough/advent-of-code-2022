4e8a754aebe2affd98f56164992ded0a7cd934ab
Ben Attenborough <ben.attenborough@featurespace.co.uk>
2023-03-01 17:33:29 +0000

    Add test input

--- src/Data/Advent12Data.elm
+++ src/Data/Advent12Data.elm
@@ -3,8 +3,12 @@ module Data.Advent12Data exposing (realInput, testInput)
 
 testInput : String
 testInput =
-    """ 
-"""
+    """Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi"""
+
 
 realInput : String
 realInput =
