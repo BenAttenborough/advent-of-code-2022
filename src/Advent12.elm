370895f73ef9f36492cb5a7930123876096d5a71
Ben Attenborough <battenborough@gmail.com>
2023-03-03 21:19:11 +0000

    Create array from input

--- src/Advent12.elm
+++ src/Advent12.elm
@@ -1,7 +1,84 @@
 module Advent12 exposing (..)
 
+import Array exposing (Array)
+import Char exposing (toCode)
+import Data.Advent12Data exposing (testInput)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Parser exposing (end)
+
+
+type Tile
+    = Start
+    | Journey
+    | End
+
+
+type alias Cell =
+    { elevation : Int
+    , cellType : Tile
+    }
+
+
+aCode : number
+aCode =
+    97
+
+
+startChar : Char
+startChar =
+    'S'
+
+
+endChar : Char
+endChar =
+    'E'
+
+
+startCharCode : Int
+startCharCode =
+    toCode startChar
+
+
+endCharCode : Int
+endCharCode =
+    toCode endChar
+
+
+startElevation : Int
+startElevation =
+    0
+
+
+endElevation : Int
+endElevation =
+    25
+
+
+charToCode : Char -> Cell
+charToCode =
+    toCode
+        >> (\code ->
+                if code == startCharCode then
+                    Cell startElevation Start
+
+                else if code == endCharCode then
+                    Cell endElevation End
+
+                else
+                    Cell (code - aCode) Journey
+           )
+
+
+prepareInput : String -> Array (Array Cell)
+prepareInput =
+    String.lines
+        >> List.map
+            (String.toList
+                >> List.map charToCode
+                >> Array.fromList
+            )
+        >> Array.fromList
 
 
 view : Html msg
--- tests/Advent12Test.elm
+++ tests/Advent12Test.elm
@@ -1,6 +1,8 @@
 module Advent12Test exposing (..)
 
 import Advent12 exposing (..)
+import Array exposing (Array)
+import Data.Advent12Data exposing (testInput)
 import Expect
 import Test exposing (..)
 
@@ -14,5 +16,60 @@ suite =
                     Expect.equal
                         1
                         1
+            , test "prepareInput" <|
+                \_ ->
+                    Expect.equal
+                        (prepareInput "aSa\nbEb")
+                        (Array.fromList
+                            [ Array.fromList
+                                [ { cellType = Journey, elevation = 0 }
+                                , { cellType = Start, elevation = startElevation }
+                                , { cellType = Journey, elevation = 0 }
+                                ]
+                            , Array.fromList
+                                [ { cellType = Journey, elevation = 1 }
+                                , { cellType = End, elevation = endElevation }
+                                , { cellType = Journey, elevation = 1 }
+                                ]
+                            ]
+                        )
+            , test "prepareInput testInput" <|
+                \_ ->
+                    -- Comparing all the records is tedious so we just compare elevations
+                    Expect.equal
+                        (prepareInput testInput |> Array.map (Array.map .elevation))
+                        (Array.fromList
+                            [ Array.fromList [ startElevation, 0, 1, 16, 15, 14, 13, 12 ]
+                            , Array.fromList [ 0, 1, 2, 17, 24, 23, 23, 11 ]
+                            , Array.fromList [ 0, 2, 2, 18, 25, endElevation, 23, 10 ]
+                            , Array.fromList [ 0, 2, 2, 19, 20, 21, 22, 9 ]
+                            , Array.fromList [ 0, 1, 3, 4, 5, 6, 7, 8 ]
+                            ]
+                        )
+            , test "charToCode a" <|
+                \_ ->
+                    Expect.equal
+                        (charToCode 'a')
+                        (Cell 0 Journey)
+            , test "charToCode b" <|
+                \_ ->
+                    Expect.equal
+                        (charToCode 'b')
+                        (Cell 1 Journey)
+            , test "charToCode z" <|
+                \_ ->
+                    Expect.equal
+                        (charToCode 'z')
+                        (Cell 25 Journey)
+            , test "charToCode S" <|
+                \_ ->
+                    Expect.equal
+                        (charToCode 'S')
+                        (Cell startElevation Start)
+            , test "charToCode E" <|
+                \_ ->
+                    Expect.equal
+                        (charToCode 'E')
+                        (Cell endElevation End)
             ]
         ]
