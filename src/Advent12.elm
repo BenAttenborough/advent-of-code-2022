211f96210d36978c1bda71736950dad94604de13
Ben Attenborough <battenborough@gmail.com>
2023-04-07 22:08:36 +0100

    Add helper types

--- src/Advent12.elm
+++ src/Advent12.elm
@@ -21,15 +21,25 @@ type alias Coordinates =
     ( Int, Int )
 
 
+type alias VisualCell =
+    ( Int, Int, Char )
+
+
+type alias Atlas =
+    Array (Array VisualCell)
+
+
+type alias UnvisitedCells =
+    Dict ( Int, Int ) Int
+
+
 type Msg
     = Advance
 
 
-type alias Model =
-    Dict ( Int, Int ) Int
 
-
-
+-- type alias Model =
+--     Dict ( Int, Int ) Int
 -- Constants, can be adjusted for different problems
 
 
@@ -101,21 +111,21 @@ coordinateY =
     Tuple.second
 
 
-findStart : Array (Array ( Int, Int, Char )) -> Maybe Coordinates
+findStart : Atlas -> Maybe Coordinates
 findStart charArray =
     charArray
         |> uniqueItemFrom2DArray (\item -> Tuple3.third item == startChar)
         |> Maybe.map (\( intX, intY, _ ) -> ( intX, intY ))
 
 
-findEnd : Array (Array ( Int, Int, Char )) -> Maybe Coordinates
+findEnd : Atlas -> Maybe Coordinates
 findEnd charArray =
     charArray
         |> uniqueItemFrom2DArray (\item -> Tuple3.third item == endChar)
         |> Maybe.map (\( intX, intY, _ ) -> ( intX, intY ))
 
 
-inputToCharArray : String -> Array (Array ( Int, Int, Char ))
+inputToCharArray : String -> Atlas
 inputToCharArray =
     String.lines
         >> List.map
@@ -136,11 +146,38 @@ array2dMap function array =
     array |> Array.map (Array.map function)
 
 
+array2dGetNode : Int -> Int -> Array (Array a) -> Maybe a
+array2dGetNode x y twoDMap =
+    twoDMap
+        |> Array.get y
+        |> Maybe.andThen (Array.get x)
 
+
+
+-- getAvailableNeighbours : VisualCell -> Atlas -> String -> List String
+-- getAvailableNeighbours cell atlas parentKey =
+--     let
+--         up =
+--             array2dGetNode (coordinateX cell) (cell.y - 1) atlas
+--                 |> Maybe.andThen (nodeTraversable cell)
+--         down =
+--             getNode cell.x (cell.y + 1) atlas
+--                 |> Maybe.andThen (nodeTraversable cell)
+--         left =
+--             getNode (cell.x - 1) cell.y atlas
+--                 |> Maybe.andThen (nodeTraversable cell)
+--         right =
+--             getNode (cell.x + 1) cell.y atlas
+--                 |> Maybe.andThen (nodeTraversable cell)
+--     in
+--     [ up, down, left, right ]
+--         |> List.filterMap identity
+--         |> List.map convertCellToKey
+--         |> List.filter (\item -> not (item == parentKey))
 -- VIEW helpers
 
 
-printCharRow : Array ( Int, Int, Char ) -> Coordinates -> Coordinates -> Dict ( Int, Int ) Int -> Html msg
+printCharRow : Array VisualCell -> Coordinates -> Coordinates -> UnvisitedCells -> Html msg
 printCharRow row start end unvisited =
     Array.toList row
         |> List.map
@@ -163,10 +200,10 @@ printCharRow row start end unvisited =
                             "cyan"
 
                         else if Dict.member ( x, y ) unvisited then
-                            "gray"
+                            "greenyellow"
 
                         else
-                            "greenyellow"
+                            "gray"
                 in
                 span [ style "color" cellStyle ] [ Html.text ((String.fromChar << Tuple3.third) item) ]
             )
@@ -176,13 +213,13 @@ printCharRow row start end unvisited =
            )
 
 
-printCharGrid : Array (Array ( Int, Int, Char )) -> Coordinates -> Coordinates -> Dict ( Int, Int ) Int -> List (Html msg)
+printCharGrid : Atlas -> Coordinates -> Coordinates -> UnvisitedCells -> List (Html msg)
 printCharGrid grid start end unvisited =
     Array.toList grid
         |> List.map (\row -> printCharRow row start end unvisited)
 
 
-checkInput : Array (Array ( Int, Int, Char )) -> Result String ( Coordinates, Coordinates )
+checkInput : Atlas -> Result String ( Coordinates, Coordinates )
 checkInput input =
     case findStart input of
         Nothing ->
@@ -211,15 +248,17 @@ view cell =
 puzzleView : String -> Html msg
 puzzleView input =
     let
-        charArray : Array (Array ( Int, Int, Char ))
+        charArray : Atlas
         charArray =
             inputToCharArray input
 
-        unvisitedCells : Dict ( Int, Int ) Int
+        unvisitedCells : UnvisitedCells
         unvisitedCells =
             charArray
                 |> array2dMap (Tuple3.third >> charToCode)
                 |> array2dToDict2d
+
+        -- |> Dict.remove ( 1, 1 )
     in
     case checkInput charArray of
         Ok ( start, end ) ->
